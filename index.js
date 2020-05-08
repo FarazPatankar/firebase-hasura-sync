f76ff8af568cda8c7b1a2c68668b1db514b0ee43
Faraz Patankar <farazpatankar@gmail.com>
2020-05-08 19:47:59 -0400

    add sign up and delete functions

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const request = require("graphql-request");
 
-exports.registerUser = functions.https.onCall(async (data, context) => {
+const client = new request.GraphQLClient(YOUR_GRAPHQL_ENDPOINT, {
+  headers: {
+    "content-type": "application/json",
+    "x-hasura-admin-secret": YOUR_ADMIN_SECRET,
+  }
+});
+
+admin.initializeApp(functions.config().firebase);
+
+exports.registerUser = functions.https.onCall(async (data) => {
   const { email, password } = data;
 
-  if (email === null || password === null || displayName === null) {
+  if (email === null || password === null) {
     throw new functions.https.HttpsError("invalid-argument", "email and password are required fields");
   }
 
@@ -30,5 +40,47 @@ exports.registerUser = functions.https.onCall(async (data, context) => {
       msg = e.message;
     }
     throw new functions.https.HttpsError(errorCode, msg, JSON.stringify(e) );
+  }
+});
+
+exports.processSignUp = functions.auth.user().onCreate(async user => {
+  const { uid: id, email } = user;
+  const mutation = `
+    mutation($id: String!, $email: String) {
+      insert_users(objects: [{
+        id: $id,
+        email: $email,
+      }]) {
+        affected_rows
+      }
+    }
+  `;
+
+  try {
+    const data = await client.request(mutation, { id, email });
+
+    return data;
+  } catch (e) {
+    throw new functions.https.HttpsError('invalid-argument', e.message);
+  }
+});
+
+exports.processDelete = functions.auth.user().onDelete(async (user) => {
+  const mutation = `
+    mutation($id: String!) {
+      delete_users(where: {id: {_eq: $id}}) {
+        affected_rows
+      }
+    }
+  `;
+  const id = user.uid;
+
+  try {
+    const data = await client.request(mutation, {
+      id: id,
+    })
+    return data;
+  } catch (e) {
+    throw new functions.https.HttpsError('invalid-argument', e.message);
   }
 });
